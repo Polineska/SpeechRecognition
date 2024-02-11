a649f0e04baba56df1a89a7206519f0496e25679
Polina <114223494+Polineska@users.noreply.github.com>
2024-02-11 21:17:54 +0300

    Update whisperPC.py
    Локальная установка

--- whisperPC.py
+++ whisperPC.py
@@ -1,1 +1,10 @@
+import whisper
 
+# Загрузка аудиофайла с использованием модели "tiny"
+
+model = whisper.load_model("small")
+
+result = model.transcribe("rep.wav")
+podcast_transcript = result["text"]
+podcast_transcript[:1000]
+print(podcast_transcript)
