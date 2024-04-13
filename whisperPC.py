330324755df9071b079367076ab3480ab523ee52
Polina <114223494+Polineska@users.noreply.github.com>
2024-04-13 15:58:33 +0300

    Update whisperPC.py
    добавлены комментарии

--- whisperPC.py
+++ whisperPC.py
@@ -1,10 +1,8 @@
+#Работа с Whisper локально. Предварительно необходимо установить соответсвтующие библеотеки на ПК, через консольную строку 
 import whisper
+model = whisper.load_model("small") #загрузка модели whisper
 
-# Загрузка аудиофайла с использованием модели "tiny"
-
-model = whisper.load_model("small")
-
-result = model.transcribe("rep.wav")
-podcast_transcript = result["text"]
+result = model.transcribe("rep.wav") #файл, который необходимо распознать. Важно файл должен находиться в одной папке с кодом
+podcast_transcript = result["text"] #сохранение расшифрованного текста
 podcast_transcript[:1000]
 print(podcast_transcript)
